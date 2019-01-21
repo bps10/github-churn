6f1e01c020d1a3ffc7a5c61b2eb9a8cf3fcf32db
Brian Schmidt <brian.patrick.schmidt@gmail.com>
2019-01-21 16:44:14 -0500

    working on CVL analysis; pyspark moved to helper; data fetch done for now

--- helper.py
+++ helper.py
@@ -1,3 +1,66 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import pandas as pd
+
+from pyspark.sql import SparkSession, udf
+from pyspark.sql.functions import to_timestamp, datediff
+from pyspark.sql.types import IntegerType, FloatType, DoubleType, BooleanType
+
+
+def get_merged_data():
+    
+    spark = SparkSession.builder.appName('gh-churn').getOrCreate()
+    first_period = spark.read.csv('events_data/events_2016_01_01_2016_06_01.csv', 
+                                  header = True, inferSchema = True)    
+    second_period = spark.read.csv('events_data/events_2016_06_02_2016_11_01_01.csv', 
+                                   header = True, inferSchema = True)
+    users = spark.read.csv('all_users.csv', header=True, inferSchema=True)
+    users = users.drop('event_count').drop('last_event').drop('first_event').drop('_c0')
+    
+    churn_data = users.join(first_period, users['login'] == first_period['actor'], 
+                            how='left')
+    second_period_event_count = second_period.selectExpr(
+        "actor as login", "event_count as second_period_event_count")
+    
+    churn_data = churn_data.join(second_period_event_count,
+                                 on='login', how='left')
+    '''
+    churn_data = churn_data.withColumn("first_event", to_timestamp(churn_data.first_event))
+    churn_data = churn_data.withColumn("last_event", to_timestamp(churn_data.last_event))
+    churn_data = churn_data.withColumn("created_at", to_timestamp(churn_data.created_at))
+    churn_data = churn_data.withColumn("updated_at", to_timestamp(churn_data.updated_at))
+    
+    churn_data = churn_data.withColumn("time_between_first_last_event", 
+                                   datediff(churn_data.last_event, churn_data.first_event))
+    '''
+    
+    churn_data = churn_data.withColumn("public_repos_count", churn_data.public_repos_count.cast(IntegerType()))
+    churn_data = churn_data.withColumn("public_gists_count", churn_data.public_gists_count.cast(IntegerType()))
+
+    churn_data = churn_data.fillna(0, subset='second_period_event_count')
+    
+    f_udf=udf.UserDefinedFunction(lambda x: 1 if x is not None else 0, IntegerType())
+
+    churn_data = churn_data.withColumn("blog", f_udf(churn_data.blog))
+    churn_data = churn_data.withColumn("company", f_udf(churn_data.company))
+    churn_data = churn_data.withColumn("hireable", f_udf(churn_data.hireable))
+    
+    churn_data = churn_data.withColumnRenamed("event_count", "frequency")
+
+    churn_data = churn_data.select('login', 'followers_count', 'following_count', 'blog',
+                   'company', 'created_at', 'public_repos_count', 'public_gists_count',
+                   'hireable',
+                   'updated_at', 'time_between_first_last_event', 'last_event', 'first_event',
+                   'frequency', 
+                   'second_period_event_count',
+                   'CommitCommentEvent_count', 'CreateEvent_count', 'DeleteEvent_count', 
+                   'ForkEvent_count', 'GollumEvent_count', 'IssueCommentEvent_count',
+                   'IssuesEvent_count', 'MemberEvent_count', 'PublicEvent_count', 
+                   'PullRequestEvent_count', 'PullRequestReviewCommentEvent_count',
+                   'PushEvent_count', 'ReleaseEvent_count', 'WatchEvent_count')
+    
+    return churn_data.toPandas()
 
 
 def get_user_info(gh, user):
@@ -70,3 +133,8 @@ def get_user_info_(user):
     body.decode('iso-8859-1')
 
     return dict(json.loads(d))
+
+
+if __name__ == '__main__':
+    df = get_merged_data()
+    print(df.head())
